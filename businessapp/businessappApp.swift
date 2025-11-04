5bc2d2d5db9636a1e08a546774ab75f3561f35ee
Smithy1232 <holypigs145@gmail.com>
2025-11-04 10:43:23 -0800

    Initial Commit

---
+++ businessapp/businessappApp.swift
@@ -0,0 +1,17 @@
+//
+//  businessappApp.swift
+//  businessapp
+//
+//  Created by Aadjot Singh Sidhu on 11/4/25.
+//
+
+import SwiftUI
+
+@main
+struct businessappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ businessappTests/businessappTests.swift
@@ -0,0 +1,17 @@
+//
+//  businessappTests.swift
+//  businessappTests
+//
+//  Created by Aadjot Singh Sidhu on 11/4/25.
+//
+
+import Testing
+@testable import businessapp
+
+struct businessappTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
